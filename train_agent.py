8bd76ab2e6943ad9b55191c55434d941853a6bf2
wuyongjun <abluysky@gmail.com>
2025-06-07 10:55:10 +0800

    <feature> Adjust training logic

--- train_agent.py
+++ train_agent.py
@@ -1,12 +1,13 @@
-from __future__ import annotations
-import argparse
-import time
 import yaml
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
 
-from scrcpy_env     import ScrcpyEnv
-from env_launcher   import ScrcpyLauncher
+
+from adb_control import AdbControl
+from scrcpy_env import ScrcpyEnv
+from scrcpy_video import VideoDecoder
+from env_launcher import ScrcpyLauncher
+from game_detector import GameDetector, GameState
 
 def main(config):
     SCREEN_DIM = config["display"]["screen"]
@@ -17,32 +18,39 @@ def main(config):
     port  = config["adb_bridge"]["port"]
     serial = config["adb_bridge"]["serial"]
 
-    steps = 1_000_000
+    k =  SCREEN_DIM[0]/FRAME_DIM[0]
 
 
     # 1️⃣ 启动 scrcpy-server 与 ADB forward
     launcher = ScrcpyLauncher(
         serial = serial,
-        video_por t= port,
+        video_port = port,
     )
 
     try:
         with launcher:
+            resize = (FRAME_DIM[0], FRAME_DIM[1])
+            decoder = VideoDecoder(host, port, resize=resize)
+            detector = GameDetector(k=k)
+            ctrl = AdbControl(serial)
+
             # 2️⃣ 创建单环境（用 DummyVecEnv 适配 SB3）
             def make_env():
                 return ScrcpyEnv(
-                    host = host,
-                    video_port = port,
-                    serial = serial,
+                    decoder,
+                    ctrl,
+                    detector,
+                    resize,
                 )
 
             vec_env = DummyVecEnv([make_env])
 
+            steps = 100
             # # 3️⃣ 训练 PPO
             model = PPO(
                 "CnnPolicy",
                 vec_env,
-                n_steps=256,
+                n_steps=steps,
                 batch_size=64,
                 learning_rate=3e-4,
                 clip_range=0.2,
@@ -50,16 +58,14 @@ def main(config):
                 verbose=1,
             )
             model.learn(total_timesteps = steps)
-            model.save("ppo_scrcpy_single")
-
+            model.save("BrawlStars")
             vec_env.close()
+
     except KeyboardInterrupt:
         print("\n[train_agent] 手动中断，已安全退出。")
 
 
-
 if __name__ == "__main__":
-
-with open('config.yaml') as f:
-    config = yaml.safe_load(f)
-    main(config)
+    with open('config.yaml') as f:
+        config = yaml.safe_load(f)
+        main(config)
